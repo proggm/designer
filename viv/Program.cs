a13f62835da1116fcfc6edc5d68b1f384ad0edfd
viktor <moiseev0506@mail.ru>
2022-10-27 21:10:17 +0300

    version 1

---
+++ viv/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person sergey = new Person ("Сергей",16);
+            sergey.Print();
+            Person viktor = new Person("Виктор", 16);
+            viktor.Print();
+            Console.ReadKey();
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+            public byte Age { get; set; }
+
+            public Person (string name, byte age)
+            {
+                this.Name = name;
+               this.Age = age;
+            }   
+            public void Print()
+            {
+                Console.WriteLine($"Name: {Name}\nAge:{Age}");
+            }
+        }
+    }
+
+}
